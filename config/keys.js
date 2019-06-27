1fdb219f9f2e5805f3784b6b9ffeefed72eaea93
Rod <rod.bromberg@yahoo.com>
2019-06-27 13:18:25 -0400

    add all files

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://rod:rod@devconnector-ggckh.mongodb.net/test?retryWrites=true&w=majority"
+};
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+// DB Config
+// importing
+const db = require("./config/keys.js").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch(err => console.log({ err, error: "Couldnt Connect to Mongo DB" }));
+
+app.get("/", (req, res) => {
+  res.send("Server is Live!");
+});
+
+const port = process.env.PORT || 9000;
+
+app.listen(port, () => console.log(`Port ${port} is live`));
